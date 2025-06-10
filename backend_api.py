6d347cd88fe1ae17a7d54d47343446ef1a2df7f5
Gittiyap <Mooijweer@proton.me>
2025-06-10 12:17:56 +0200

    Update backend_api.py

--- backend_api.py
+++ backend_api.py
@@ -3,8 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 
 app = FastAPI()
-
-# CORS openzetten voor lokale frontend tests
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -16,5 +14,4 @@ app.add_middleware(
 @app.get("/api/meldingen")
 def get_meldingen():
     df = pd.read_csv("m2m_meldingen_logisch.csv")
-    # Kies alleen de kolommen die je wilt tonen
     return df.to_dict(orient="records")
